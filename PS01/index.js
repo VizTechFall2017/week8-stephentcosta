61f4ca7c429648b4123a7f02bac9282b195857f1
stephentcosta <31608698+stephentcosta@users.noreply.github.com>
2017-11-03 23:58:35 -0400

    Map plotted but small
    and NOT on the SVGbox1 --- UGH

--- PS01/index.js
+++ PS01/index.js
@@ -3,36 +3,79 @@ var height = document.getElementById('svg1').clientHeight;
 
 var marginLeft = 0;
 var marginTop = 0;
-
+//
 var svg = d3.select('svg')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-//set up the projection for the map
-var albersProjection = d3.geoAlbersUsa()  //tell it which projection to use
-    .scale(50000)                           //tell it how big the map should be
-    .translate([(width/2), (height/2)]);  //set the center of the map to show up in the center of the screen
+// d3.json("bostonNeighborhoods.geojson", function (data) {
+//
+//   var group = svg.selectAll("g")
+//       .data(data.features)
+//       .enter()
+//       .append("g")
+//
+//   var projection = d3.geoAlbersUsa();
+//   var path = d3.geo.path().projection(projection);
+//
+//   var areas = group.append("path")
+//       .attr("d", path)
+//       .attr("class", "area")
+//       .attr("fill", "steelblue");
+//
+// });
+// // //set up the projection for the map
+// // var albersProjection = d3.geoAlbersUsa()  //tell it which projection to use
+// //     .scale(10,000)                           //tell it how big the map should be
+// //     .translate([(width/2), (height/2)]);  //set the center of the map to show up in the center of the screen
+// //
+// // //set up the path generator function to draw the map outlines
+// // path = d3.geoPath()
+// //     .projection(albersProjection);        //tell it to use the projection that we just made to convert lat/long to pixels
+// //
+// // var areaLookup = d3.map();
+// //
+// // var colorScale = d3.scaleLinear().range(['white','blue']);
+// //
+// queue()
+//     .defer(d3.json, "./bostonNeighborhoods.json")
+//     .defer(d3.csv, "./affordableUnits.csv")
+//     .await(function(err, mapData, unitsData){
+//
+//     unitsData.forEach(function(d){
+//         areaLookup.set(d.area, d.units);
+//     });
+//
+// //
+// //     colorScale.domain([0, d3.max(unitsData.map(function(d){return +d.units}))]);
+// //
 
-//set up the path generator function to draw the map outlines
-path = d3.geoPath()
-    .projection(albersProjection);        //tell it to use the projection that we just made to convert lat/long to pixels
 
-var nameLookup = d3.map();
+var canvas = d3.select("body").append("svg")
+    .attr("width", 700)
+    .attr("height", 700)
 
-var colorScale = d3.scaleLinear().range(['white','blue']);
+d3.json("bostonNeighborhoods.json", function (data) {
 
-queue()
-    .defer(d3.json, "./boston.json")
-    .defer(d3.csv, "./affordableUnits.csv")
-    .await(function(err, mapData, unitsData){
+    var group = canvas.selectAll("g")
+        .data(data.features)
+        .enter()
+        .append("g")
+
+    var projection = d3.geoAlbers()
+        .scale(700)
+        .translate([(width/2), (height/2)]);
+
+    path = d3.geoPath()
+          .projection(projection);
+
+    var area = group.append("path")
+        .attr("d", path)
+        .attr("class", "area")
+        .attr("fill", "black")
 
 
-    unitsData.forEach(function(d){
-        nameLookup.set(d.area, d.units);
-    });
-
-
-    colorScale.domain([0, d3.max(unitsData.map(function(d){return +d.units}))]);
+});
 
     svg.selectAll("path")               //make empty selection
         .data(mapData.features)          //bind to the features array in the map data
@@ -41,9 +84,7 @@ queue()
         .attr("d", path)                //actually draw them
         .attr("class", "feature")
         .attr('fill',function(d){
-            return colorScale(nameLookup.get(d.properties.NAME));
+            return colorScale(areaLookup.get(d.properties.NAME));
         })
-        .attr('stroke','white')
+        .attr('stroke','black')
         .attr('stroke-width',.2);
-
-  });
