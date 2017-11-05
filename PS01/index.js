cd9c561cbb20145b2fb0de6ef0bf840204e4b024
stephentcosta <31608698+stephentcosta@users.noreply.github.com>
2017-11-04 22:38:54 -0400

    got colorcscale to COLOR
    colorscale is not writing the fill for the areas but not SCALING them past on units data...

--- PS01/index.js
+++ PS01/index.js
@@ -8,7 +8,62 @@ var svg = d3.select('svg')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-// var areaLookup = d3.map(mapData, function(d){
+var projection = d3.geoAlbers()
+    .scale( 190000 )
+    .rotate( [71.057,0] )
+    .center( [0, 42.313] )
+    .translate( [width/2,height/2] );
+
+    path = d3.geoPath()
+          .projection(projection);
+
+var stateLookup = d3.map();
+
+var colorScale = d3.scaleLinear().range(["white","blue"]);
+
+// var group = svg.selectAll("g")
+//     .data(data.features)
+//     .enter()
+//     .append("g")
+
+// var area = group.append("path")
+//     .attr("d", path)
+//     .attr("class", "area")
+//     .attr("stroke", "white")
+//     .attr("fill", "#00a6b4")
+
+queue()
+    .defer(d3.json, "./bostonNeighborhoods.json")
+    .defer(d3.csv, "./affordableUnits.csv")
+    .await(function(err, mapData, unitsData){
+
+    unitsData.forEach(function(d){
+        stateLookup.set(d.area, d.units);
+    });
+
+    colorScale.domain([0, d3.max(unitsData.map(function(d){return +d.units}))]);
+
+        svg.selectAll("path")               //make empty selection
+            .data(mapData.features)          //bind to the features array in the map data
+            .enter()
+            .append("path")                 //add the paths to the DOM
+            .attr("d", path)                //actually draw them
+            .attr("class", "feature")
+            .attr('fill',function(d){
+                return colorScale(stateLookup.get(d.properties.NAME));
+            })
+            .attr('stroke','white')
+            .attr('stroke-width',.2);
+
+      });
+
+    // d3.json("./bostonNeighborhoods.json", function (data) {
+
+
+            // .attr('fill', function(d) { return colorScale(d.units); });
+
+
+// var areaLookup = d3.map(areaData, function(d){
 //   return d.units
 // });
 // //
@@ -16,7 +71,7 @@ var svg = d3.select('svg')
 // //
 // queue()
 //     .defer(d3.json, "./bostonNeighborhoods.json")
-//     .defer(d3.csv, "./affordableUnits.csv")
+//     .defer(d3.csv, "./affordableUnits.csv";)
 //     .await(function(err, mapData, unitsData){
 //
 //     unitsData.forEach(function(d){
@@ -25,7 +80,7 @@ var svg = d3.select('svg')
 //
 // //
 //     colorScale.domain([0, d3.max(unitsData.map(function(d){return +d.units}))]);
-// //
+//
 
 //
 // var canvas = d3.select("body").append("svg")
@@ -33,6 +88,10 @@ var svg = d3.select('svg')
 //     .attr("height", 700)
 
 // var usChart = dc.geoChoroplethChart("#map");
+
+// var colorScale = d3.scale.threshold()
+//         .domain([0, 2000, 4000, 6000, 8000, 10000, 12000])
+//          .range(["#cccccc", "#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f",]);
 
 // var color = d3.scale.threshold()
 //       .domain([1000, 3000, 5000, 7000, 9000, 11000, 13000])
@@ -50,27 +109,7 @@ var svg = d3.select('svg')
 //   var facts = crossfilter(data);
 // })
 
-d3.json("./bostonNeighborhoods.json", function (data) {
 
-    var group = svg.selectAll("g")
-        .data(data.features)
-        .enter()
-        .append("g")
-
-        var projection = d3.geoAlbers()
-        .scale( 190000 )
-        .rotate( [71.057,0] )
-        .center( [0, 42.313] )
-        .translate( [width/2,height/2] );
-
-    path = d3.geoPath()
-          .projection(projection);
-
-    var area = group.append("path")
-        .attr("d", path)
-        .attr("class", "area")
-        .attr("stroke", "white")
-        .attr("fill", "#00a6b4")
 
         // .attr("stroke", "white")
         // .attr("fill", "#00a6b4")
@@ -82,7 +121,7 @@ d3.json("./bostonNeighborhoods.json", function (data) {
         // })
 
 
-});
+// });
     //
     // svg.selectAll("path")               //make empty selection
     //     .data(mapData.features)          //bind to the features array in the map data
